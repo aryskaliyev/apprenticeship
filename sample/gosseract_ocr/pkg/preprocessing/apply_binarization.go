6b884aa8609222a5ebc0fadca6260a64e99775a7
aryskaliyev <aryskaliyev@nu.edu.kz>
2024-03-31 16:16:46 +0600

    Updating cv to gocv

--- sample/gosseract_ocr/pkg/preprocessing/apply_binarization.go
+++ sample/gosseract_ocr/pkg/preprocessing/apply_binarization.go
@@ -11,8 +11,11 @@ func ApplyBinarization(blurred gocv.Mat) (gocv.Mat, error) {
 	}
 	defer blurred.Close()
 
+	thresh := 0
+	maxval := 255
+
 	binary := gocv.NewMat()
-	gocv.Threshold(blurred, binary, 0, 255, gocv.ThresholdBinary)
+	gocv.Threshold(blurred, &binary, thresh, maxval, gocv.ThresholdBinary)
 
 	if binary == nil {
 		return gocv.Mat{}, fmt.Errorf("failed to apply binarization")
